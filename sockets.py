47dcd81f77d37df7512d5172f87f12fef350f60f
Mariela501 <116614480+Mariela501@users.noreply.github.com>
2022-11-20 20:56:44 -0600

    Update sockets.py

--- sockets.py
+++ sockets.py
@@ -1,27 +1,22 @@
 import socket
 
-host = socket.gethostname() # Esta función nos da el nombre de la máquina
+host = socket.gethostname() # Se obtiene el nombre de la máquina
 port = 12345
-BUFFER_SIZE = 1024 # Usamos un número pequeño para tener una respuesta rápida 
+BUFFER_SIZE = 1024 
 
-'''Los objetos socket soportan el context manager type
-así que podemos usarlo con una sentencia with, no hay necesidad
-de llamar a socket_close()
-'''
-# Creamos un objeto socket tipo TCP
+
+# Se crea un objeto socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
 
     socket_tcp.bind((host, port)) 
-    socket_tcp.listen(5) # Esperamos la conexión del cliente 
-    conn, addr = socket_tcp.accept() # Establecemos la conexión con el cliente 
+    socket_tcp.listen(5) # Se establecen los segundos de espera  
+    conn, addr = socket_tcp.accept() # Conexión con el cliente 
     with conn:
         print('[*] Conexión establecida') 
         while True:
-            # Recibimos bytes, convertimos en str
             data = conn.recv(BUFFER_SIZE)
-            # Verificamos que hemos recibido datos
             if not data:
                 break
             else:
                 print('[*] Datos recibidos: {}'.format(data.decode('utf-8'))) 
-            conn.send(data) # Hacemos echo convirtiendo de nuevo a bytes
+            conn.send(data)
